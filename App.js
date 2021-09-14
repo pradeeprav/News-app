ce568088bc9f4d98ac20ff918178d04364d9a8db
pradeeprav <87473106+pradeeprav@users.noreply.github.com>
2021-09-14 21:28:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// component impoer 
+import Main  from './Components/Main';
+
+function App() {
+  return (
+    <div className="App">
+    <Main />
+     
+    </div>
+  );
+}
+
+export default App;
